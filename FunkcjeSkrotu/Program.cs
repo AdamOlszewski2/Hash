eb393f792f8863f42f2f17b62b0747ac095b52bb
Adam Adam <sq3swg@outlook.com>
2019-04-24 20:50:57 +0200

    Add project files.

---
+++ FunkcjeSkrotu/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunkcjeSkrotu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            md5Class md5 = new md5Class();
+            Console.WriteLine(md5Class.MD5Hash("akjsdkjsahd"));
+        }
+    }
+}
